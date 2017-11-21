f74a6ee47639204df093b04f63c44efa07f8941e
XiaZhengXin <xzx@xiazhengxin.name>
2017-11-21 15:15:49 +0800

    mysqldump.exe is version 5.7.17, but the MySQL Server to be dumped has version 5.7.18. Because the version of mysqldump is older than the server, some features may not be backed up properly. It is recommended you upgrade your local MySQL client programs, including mysqldump, to a version equal to or newer than that of the target server. The path to the dump tool must then be set in Preferences -> Administrator -> Path to mysqldump Tool: 15:12:54 Dumping user_center (all tables) Running: mysqldump.exe --defaults-file="c:\users\admini~1\appdata\local\temp\tmpzsvdhd.cnf"  --user=root --host=localhost --protocol=tcp --port=3306 --default-character-set=utf8 --no-data --skip-triggers "user_center" 15:12:54 Export of C:\Users\Administrator\Documents\dumps\Dump20171121.sql has finished
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- database/db.sql
+++ database/db.sql
@@ -1,63 +1,137 @@
-CREATE TABLE `user_center`.`account` (
-  `account_id`    BIGINT(20)  NOT NULL,
-  `user_key`      VARCHAR(45) NULL,
-  `password`      VARCHAR(45) NULL,
-  `account_type`  TINYINT(5)  NULL,
-  `union_user_id` BIGINT(20)  NULL,
-  `status`        TINYINT(5)  NULL,
+CREATE DATABASE  IF NOT EXISTS `user_center` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `user_center`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: user_center
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account` (
+  `account_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_key` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `account_type` tinyint(5) DEFAULT NULL,
+  `union_user_id` bigint(20) DEFAULT NULL,
+  `status` tinyint(5) DEFAULT NULL,
   PRIMARY KEY (`account_id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `channel`
+--
 
-CREATE TABLE `user_center`.`user` (
-  `user_id`     BIGINT(20)  NOT NULL,
-  `telephone`   VARCHAR(45) NULL,
-  `nickname`    VARCHAR(45) NULL,
-  `avatar`      VARCHAR(45) NULL,
-  `birthday`    DATE        NULL,
-  `sex`         TINYINT(5)  NULL,
-  `signature`   VARCHAR(45) NULL,
-  `user_source` VARCHAR(45) NULL,
-  `role`        TINYINT(5)  NULL,
-  `status`      TINYINT(5)  NULL,
-  `create_time` DATETIME    NULL,
-  `modify_time` DATETIME    NULL,
-  PRIMARY KEY (`user_id`)
-);
-
-
-CREATE TABLE `user_center`.`channel` (
-  `channel_id`       BIGINT(20)   NOT NULL,
-  `channel_name`     VARCHAR(45)  NULL,
-  `channel_key`      VARCHAR(45)  NULL,
-  `channel_secret`   VARCHAR(45)  NULL,
-  `create_time`      DATETIME     NULL,
-  `pay_callback_url` VARCHAR(100) NULL,
-  `is_test`          TINYINT(5)   NULL,
+DROP TABLE IF EXISTS `channel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `channel` (
+  `channel_id` bigint(20) NOT NULL,
+  `channel_name` varchar(45) DEFAULT NULL,
+  `channel_key` varchar(45) DEFAULT NULL,
+  `channel_secret` varchar(45) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `pay_callback_url` varchar(100) DEFAULT NULL,
+  `is_test` tinyint(5) DEFAULT NULL,
   PRIMARY KEY (`channel_id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `mapping`
+--
 
-CREATE TABLE `user_center`.`mapping` (
-  `mapping_id`  BIGINT(20)  NOT NULL,
-  `channel_id`  BIGINT(20)  NULL,
-  `channel_uid` VARCHAR(45) NULL,
-  `user_id`     BIGINT(20)  NULL,
-  `create_time` DATETIME    NULL,
+DROP TABLE IF EXISTS `mapping`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mapping` (
+  `mapping_id` bigint(20) NOT NULL,
+  `channel_id` bigint(20) DEFAULT NULL,
+  `channel_uid` varchar(45) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`mapping_id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `sms_code`
+--
 
-CREATE TABLE `user_center`.`token` (
-  `user_id` BIGINT(20)  NULL,
-  `token`   VARCHAR(45) NULL,
-  `ttl`     BIGINT(20)  NULL
-);
-
-
-CREATE TABLE `user_center`.`sms_code` (
-  `telephone` BIGINT(20) NOT NULL,
-  `code`      INT        NULL,
-  `ttl`       BIGINT(20) NULL,
+DROP TABLE IF EXISTS `sms_code`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sms_code` (
+  `telephone` bigint(20) NOT NULL,
+  `code` int(11) DEFAULT NULL,
+  `ttl` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`telephone`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `token` (
+  `user_id` bigint(20) NOT NULL,
+  `token` varchar(45) DEFAULT NULL,
+  `ttl` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` bigint(20) NOT NULL,
+  `telephone` varchar(45) DEFAULT NULL,
+  `nickname` varchar(45) DEFAULT NULL,
+  `avatar` varchar(45) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `sex` tinyint(5) DEFAULT NULL,
+  `signature` varchar(45) DEFAULT NULL,
+  `user_source` varchar(45) DEFAULT NULL,
+  `role` tinyint(5) DEFAULT NULL,
+  `status` tinyint(5) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `modify_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-21 15:12:54
