e91e4f977b957adec6f49529d6121c81ecf2cb77
XiaZhengXin <xzx@xiazhengxin.name>
2017-12-07 15:37:10 +0800

    Lumen with CORS and OPTIONS requests see:https://gist.github.com/danharper/06d2386f0b826b669552
    bugfix...
    
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- app/Http/Middleware/CrossMiddleware.php
+++ app/Http/Middleware/CrossMiddleware.php
@@ -15,10 +15,23 @@ class CrossMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
+
         $response = $next($request);
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
-        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Origin', '*');
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
         return $response;
     }
 }
--- app/Providers/CatchAllOptionsRequestsProvider.php
+++ app/Providers/CatchAllOptionsRequestsProvider.php
@@ -15,12 +15,12 @@ class CatchAllOptionsRequestsProvider extends ServiceProvider
 {
     public function register()
     {
-        $request = app('request');
-
-        if ($request->isMethod('OPTIONS')) {
-            app()->options($request->path(), function () {
-                return response('', 200);
-            });
-        }
+//        $request = app('request');
+//
+//        if ($request->isMethod('OPTIONS')) {
+//            app()->options($request->path(), function () {
+//                return response('', 200);
+//            });
+//        }
     }
 }
